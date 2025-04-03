34e5311fd470813c18d2c34691669fb64b2afff6
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-04-03 07:15:51 +0530

    Add files via upload

---
+++ PATTERNS/Npatter7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    for (int i=1; i<=5;i++)
+    {
+        for (int j=1; j<=5;j++)
+        {
+          if (i==j||j+i==6)
+          {
+            printf("1");
+          }
+          else
+          {
+            printf("0");
+          }
+          
+        }
+        printf("\n");
+        
+    }
+    
+}
