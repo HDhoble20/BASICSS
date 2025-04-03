d39a8a009595b56a966afe2e054985e32816f98f
Himanshu Dhoble <120731707+HDhoble20@users.noreply.github.com>
2025-04-03 07:20:26 +0530

    Add files via upload

---
+++ PATTERNS/p10.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+  
+    
+    for (int i = 1; i <= 5; i++)
+    {   
+        for (int k = 1; k <=6-i; k++)
+        {
+            printf(" ");
+        }
+        
+        for(int j=1;j<=5;j++)
+        {
+           if (i==1||i==5||j==1||j==5)
+           {
+              printf("*");
+           }
+           else
+           {
+            printf(" ");
+           }
+           
+        
+        
+
+        }
+    
+    
+       printf("\n");
+
+    }
+      
+}
